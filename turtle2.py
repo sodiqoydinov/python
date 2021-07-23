6d6f4d9df849cd5b8ca6d37415c6fd142deabae6
Sodiq <86997572+sodiqoydinov@users.noreply.github.com>
2021-07-23 11:26:15 +0500

    turtle2

---
+++ turtle2.py
@@ -0,0 +1,12 @@
+"""
+github:https://github.com/sodiqoydinov
+"""
+import turtle
+import time
+aylana = turtle.Turtle()
+aylana.width(10)
+aylana.color('red')
+aylana.circle(100)
+aylana.color('blue')
+aylana.circle(-100) 
+time.sleep(5)
