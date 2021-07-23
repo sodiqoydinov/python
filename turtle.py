e53e964586df46cfafe3a2288ac3921e756ed1be
Sodiq <86997572+sodiqoydinov@users.noreply.github.com>
2021-07-23 11:19:56 +0500

    turtle
    turtle moduliga misollar

---
+++ turtle.py
@@ -0,0 +1,41 @@
+"""
+github:https://github.com/sodiqoydinov
+"""
+import turtle
+import time
+
+qalamcha = turtle.Turtle()
+
+
+qalamcha.color('red') # rang 
+qalamcha.width(5) # kenglik yani chiziq kengligi
+ 
+
+qalamcha.forward(40) # Oldinga yurish yani chiziq chizb yuradi
+qalamcha.left(90) # burilish nuqtasi 90 gradus 
+
+#shaklimiz burilish nuqtasi va qadamdan iborat shuning uchun forward (qadam va burilish nuqtasi o'lchovidan foydalanamiz)
+
+qalamcha.forward(100)
+qalamcha.left(90)
+qalamcha.forward(40)
+qalamcha.right(90)
+qalamcha.forward(40)
+qalamcha.left(90)
+qalamcha.forward(100)
+qalamcha.left(90)
+qalamcha.forward(40)
+qalamcha.right(90)
+qalamcha.forward(40)
+qalamcha.left(90)
+qalamcha.forward(100)
+qalamcha.left(90)
+qalamcha.forward(40)
+qalamcha.right(90)
+qalamcha.forward(40)
+qalamcha.left(90)
+qalamcha.forward(100)
+qalamcha.left(90)
+qalamcha.forward(40)
+time.sleep(1)#Uxlash vaqti 5 sekund bu unchalik shart emas
+# back orqaga qaytaradi
